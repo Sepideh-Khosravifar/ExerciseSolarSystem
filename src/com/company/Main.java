9ab63267cdab271cac884d152acf70919d741105
sisikhosravi <sisikhosravi2021@gmail.com>
2022-05-19 14:41:02 -0400

    Solar System basic project is ready

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SolarSystem FactorX = new SolarSystem();
+        FactorX.Sun = "1";
+        FactorX.Moon = "4";
+        FactorX.Planets = "2";
+        FactorX.Stars = "10000";
+
+
+
+
+    }
+}
